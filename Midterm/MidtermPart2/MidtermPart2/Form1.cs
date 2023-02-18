05411266383abfa7b5346a243d95bee95d994abf
Timothy McMasterse <tmmcmasters@gmail.com>
2023-02-18 09:29:46 -0500

    Uploading Midterm Program using Visual STudio

---
+++ Midterm/MidtermPart2/MidtermPart2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MidtermPart2
+{
+    public partial class Form1 : Form
+    {
+        DataTable table;
+       //^^Created a data table here
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            if(index>-1)
+            {
+                txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+                //Indexes the text in the textboxes if you click on save button
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(String));
+            table.Columns.Add("Messages", typeof(String));
+            //Creates the column titles and decides what kind of data it can recieve.
+
+            dataGridView1.DataSource = table;
+
+            dataGridView1.Columns["Messages"].Visible = false;
+            dataGridView1.Columns["Title"].Width = 140;
+            //Shortens the data table width and doesn't show the messages table
+        }
+
+        private void bttNew_Click(object sender, EventArgs e)
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            //If you click on the new button, it will clear the two textboxes. 
+        }
+
+        private void bttSave_Click(object sender, EventArgs e)
+        {
+            table.Rows.Add(txtTitle.Text, txtMessage.Text);
+            //Adds the stuff to the datatable
+
+            txtTitle.Clear();
+            txtMessage.Clear();
+            //Clears after it saves the stuff to the datatable
+        }
+
+        private void bttDelete_Click(object sender, EventArgs e)
+        {
+            int index = dataGridView1.CurrentCell.RowIndex;
+
+            table.Rows[index].Delete();
+            //Delets the index text on the data table rows.
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
