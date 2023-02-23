7fb905681c6d2d590154dc61e4bb7ead80163aa0
Timothy McMasterse <tmmcmasters@gmail.com>
2023-02-23 17:40:18 -0500

    Testing Commit

---
+++ Group Project/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.categoryColum = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.itemColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.unitPriceColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.totalEstimatedValueColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.button1.Location = new System.Drawing.Point(40, 346);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(414, 346);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(750, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 51);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Load(-_/*-*\\_-)";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.categoryColum,
+            this.itemColumn,
+            this.quantityColumn,
+            this.unitPriceColumn,
+            this.totalEstimatedValueColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(900, 304);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick_1);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            // 
+            // categoryColum
+            // 
+            this.categoryColum.HeaderText = "Category";
+            this.categoryColum.MinimumWidth = 6;
+            this.categoryColum.Name = "categoryColum";
+            this.categoryColum.Width = 125;
+            // 
+            // itemColumn
+            // 
+            this.itemColumn.HeaderText = "Item";
+            this.itemColumn.MinimumWidth = 6;
+            this.itemColumn.Name = "itemColumn";
+            this.itemColumn.Width = 125;
+            // 
+            // quantityColumn
+            // 
+            this.quantityColumn.HeaderText = "Quantity";
+            this.quantityColumn.MinimumWidth = 6;
+            this.quantityColumn.Name = "quantityColumn";
+            this.quantityColumn.Width = 125;
+            // 
+            // unitPriceColumn
+            // 
+            this.unitPriceColumn.HeaderText = "Unit Price";
+            this.unitPriceColumn.MinimumWidth = 6;
+            this.unitPriceColumn.Name = "unitPriceColumn";
+            this.unitPriceColumn.Width = 125;
+            // 
+            // totalEstimatedValueColumn
+            // 
+            this.totalEstimatedValueColumn.HeaderText = "Total Estimated Value";
+            this.totalEstimatedValueColumn.MinimumWidth = 6;
+            this.totalEstimatedValueColumn.Name = "totalEstimatedValueColumn";
+            this.totalEstimatedValueColumn.ReadOnly = true;
+            this.totalEstimatedValueColumn.Width = 125;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Comic Sans MS", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(305, 412);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(363, 45);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "Incorrect Input Type";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(924, 480);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn categoryColum;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn unitPriceColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn totalEstimatedValueColumn;
+    }
+}
+
---
+++ Group Project/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Hide();
+
+            //Drop Down column attempt
+            DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+            comboBoxColumn.HeaderText = "Options";
+            comboBoxColumn.Name = "optionsColumn";
+            comboBoxColumn.Items.AddRange("Option 1", "Option 2", "Option 3");
+
+            dataGridView1.Columns.Add(comboBoxColumn);
+
+            DataGridViewComboBoxCell comboBoxCell = new DataGridViewComboBoxCell();
+            comboBoxCell.DataSource = new string[] { "Option 1", "Option 2", "Option 3" };
+            dataGridView1.Rows[0].Cells[0] = comboBoxCell;
+
+
+
+        }
+        private void dataGridView_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0) // assuming column 0 is the drop-down column
+            {
+                DataGridViewComboBoxCell comboBoxCell = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex] as DataGridViewComboBoxCell;
+                string selectedValue = comboBoxCell.Value.ToString();
+                // do something with selectedValue
+            }
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+                  
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            // Checks if the current cell is in the quantity column
+            if (dataGridView1.Columns[e.ColumnIndex].Name.Equals("quantityColumn", StringComparison.OrdinalIgnoreCase))
+            {
+                // Tries to parse the entered value as a number
+                decimal value;
+                bool isNumeric = decimal.TryParse(e.FormattedValue.ToString(), out value);
+
+                if (isNumeric)
+                {
+                    // If the entered value is numeric, hide textBox1
+                    textBox1.Hide();
+                }
+                else
+                {
+                    // If the entered value is non-numeric, show textBox1
+                    textBox1.Show();
+                }
+                if (e.FormattedValue.ToString() == "")
+                {
+                    textBox1.Hide();
+                }
+            }
+            if (dataGridView1.Columns[e.ColumnIndex].Name.Equals("unitPriceColumn", StringComparison.OrdinalIgnoreCase)) 
+            {
+                // Tries to parse the entered value as a number
+
+                decimal value;
+                bool isNumeric = decimal.TryParse(e.FormattedValue.ToString(), out value);
+                if (isNumeric)
+                {
+                    // If the entered value is numeric, hide textBox1
+                    textBox1.Hide();
+                }
+                else
+                {
+                    // If the entered value is non-numeric, show textBox1
+                    textBox1.Show();
+                }
+                if (e.FormattedValue.ToString() == "")
+                {
+                    textBox1.Hide();
+                }
+            }
+        }
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e) {
+            int quanity, item;
+            if (int.TryParse(dataGridView1.Rows[e.RowIndex].Cells["quanity"].Value.ToString(),
+                out quanity) && int.TryParse(dataGridView1.Rows[e.RowIndex].Cells["item"].Value.ToString(), out item))
+            {
+                int totalEstimatedValueColumn = quanity * item;
+                dataGridView1.Rows[e.RowIndex].Cells["totalEstimatedValueColumn"].Value = totalEstimatedValueColumn.ToString();
+            }                
+
+
+
+        }
+
+      
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
